6a2afa768be3198eb834a099468633bdc3307acb
ro77en <aluno17@aceleramaker.com.br>
2025-03-25 16:52:44 -0300

    test: add unit tests to cover open/mark fields logic

--- src/test/java/br/com/aceleramaker/model/FieldTest.java
+++ src/test/java/br/com/aceleramaker/model/FieldTest.java
@@ -1,10 +1,10 @@
 package br.com.aceleramaker.model;
 
+import br.com.aceleramaker.exception.ExplosionException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class FieldTest {
 
@@ -76,6 +76,78 @@ public class FieldTest {
         Field neighbor = new Field(3, 3);
         boolean res = field.addNeighbor(neighbor);
         assertFalse(res);
+    }
+
+    @Test
+    void testIsMarkedDefaultValue() {
+        assertFalse(field.isMarked());
+    }
+
+    @Test
+    void testSwitchIsMarked() {
+        field.switchMarkedField();
+        assertTrue(field.isMarked());
+    }
+
+    @Test
+    void testSwitchIsMarkedTwice() {
+        field.switchMarkedField();
+        field.switchMarkedField();
+        assertFalse(field.isMarked());
+    }
+
+    @Test
+    void testOpenNotMinedNotMarked() {
+        assertTrue(field.openField());
+    }
+
+    @Test
+    void testOpenNotMinedMarked() {
+        field.switchMarkedField();
+        assertFalse(field.openField());
+    }
+
+    @Test
+    void testOpenMinedMarked() {
+        field.switchMarkedField();
+        field.mine();
+        assertFalse(field.openField());
+    }
+
+    @Test
+    void testOpenMinedNotMarked() {
+        field.mine();
+        assertThrows(ExplosionException.class, () -> field.openField());
+    }
+
+    @Test
+    void testOpenNeighbors() {
+        Field field11 = new Field(1,1);
+        Field field22 = new Field(2,2);
+
+        field22.addNeighbor(field11);
+        field.addNeighbor(field22);
+
+        field.openField();
+
+        assertTrue(field22.isOpened() && field11.isOpened());
+    }
+
+    @Test
+    void testOpenMinedNeighbor() {
+        Field field22 = new Field(2,2);
+        Field field12 = new Field(1,2);
+        Field field11 = new Field(1,1);
+
+        field12.mine();
+
+        field.addNeighbor(field22);
+        field22.addNeighbor(field11);
+        field22.addNeighbor(field12);
+
+        field.openField();
+
+        assertTrue(field22.isOpened() && !field11.isOpened());
     }
 
 }
