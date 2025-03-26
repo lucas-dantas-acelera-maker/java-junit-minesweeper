7b2583f0d463d0a032d9283135a8993948ac7e85
ro77en <aluno17@aceleramaker.com.br>
2025-03-26 13:34:43 -0300

    test: add Board class junit tests

---
+++ src/test/java/br/com/aceleramaker/model/BoardTest.java
@@ -0,0 +1,76 @@
+package br.com.aceleramaker.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class BoardTest {
+
+    private Board board;
+    private static final int ROWS = 5;
+    private static final int COLS = 5;
+    private static final int MINES = 5;
+
+    @BeforeEach
+    void startBoard() {
+        board = new Board(ROWS, COLS, MINES);
+    }
+
+    @Test
+    void testBoardConstructor() {
+        assertNotNull(board, "Board should be initialized");
+    }
+
+    @Test
+    void testGenerateFields() {
+        int expectedSize = 5 * 5;
+        assertEquals(expectedSize, board.geCols() * board.getRows());
+    }
+
+    @Test
+    void testDefineNeighbors() {
+        assertTrue(
+                board.getFields().stream()
+                                .noneMatch(field -> field.getNeighboringFields()
+                                .isEmpty())
+        );
+    }
+
+    @Test
+    void testMineFields() {
+        assertEquals(5, board.getMines());
+    }
+
+    @Test
+    void testGameWon() {
+        board.getFields().stream()
+                .filter(field -> !field.isMined())
+                .forEach(Field::openField);
+
+        assertTrue(board.goalAchieved());
+    }
+
+    @Test
+    void testRestartBoard() {
+        board.getFields()
+                .forEach(field -> {
+                    if (!field.isMined()) {
+                        field.openField();
+                    } else {
+                        field.switchMarkedField();
+                    }
+                });
+
+        board.restartBoard();
+
+        boolean isBoardReseted = board.getFields().stream()
+                .allMatch(field -> !field.isOpened() && !field.isMarked());
+
+        assertTrue(isBoardReseted);
+
+        assertEquals(MINES, board.getMines());
+    }
+
+}
