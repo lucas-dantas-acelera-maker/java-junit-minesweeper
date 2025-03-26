0252d6bd4358c7bb60fcae13b26ba5cf92def256
ro77en <aluno17@aceleramaker.com.br>
2025-03-25 21:30:07 -0300

    docs: add Field class JavaDoc

--- src/main/java/br/com/aceleramaker/model/Field.java
+++ src/main/java/br/com/aceleramaker/model/Field.java
@@ -5,6 +5,14 @@ import br.com.aceleramaker.exception.ExplosionException;
 import java.util.ArrayList;
 import java.util.List;
 
+
+/**
+ * <p>
+ * This class represents a single field of the Board
+ * </p>
+ * Each field has a position determined by row and column,
+ * can be mined and can be opened or marked.
+ * */
 public class Field {
     private final int row;
     private final int column;
@@ -15,11 +23,27 @@ public class Field {
 
     private List<Field> neighboringFields = new ArrayList<>();
 
+    /**
+     * <p>
+     * Constructs a field with position based on row x column.
+     * </p>
+     * @param row field's row.
+     * @param column field's column.
+     * */
     Field(int row, int column) {
         this.row = row;
         this.column = column;
     }
 
+    /**
+     * <p>
+     * Adds a neighbor to the field.
+     * </p>
+     * The field is valid as a neighbor if the distance from the original
+     * is equal to 1 for top, left, right, bottom fields, or 2 for diagonal.
+     * @param neighbor the adjacent field to be added as neighbor.
+     * @return true if the neighbor is added, otherwise returns false,
+     * */
     boolean addNeighbor(Field neighbor) {
         boolean diffRow = this.row != neighbor.row;
         boolean diffCol = this.column != neighbor.column;
@@ -45,12 +69,29 @@ public class Field {
         return false;
     }
 
+    /**
+     * <p>
+     * Switches the marked state.
+     * </p>
+     * */
     void switchMarkedField() {
         if (!isOpen) {
             isMarked = !isMarked;
         }
     }
 
+    /**
+     * <p>
+     * Opens the field.
+     * </p>
+     * Verifies if the field is already opened or marked and if
+     * so, the neighbor fields are opened as well.
+     *
+     * @return true if the selected field has been opened,
+     * otherwise returns false.
+     * @throws ExplosionException if the selected field is mined.
+     *
+     * */
     boolean openField() {
         if (!isOpen && !isMarked) {
             isOpen = true;
@@ -68,21 +109,46 @@ public class Field {
         return false;
     }
 
+    /**
+     * <p>
+     * Verifies if neighbor fields are safe.
+     * </p>
+     * @return true if the neighborhood is safe, otherwise returns false.
+     * */
     boolean isNeighborhoodSafe() {
         return neighboringFields.stream()
                 .noneMatch(nField -> nField.hasMine);
     }
 
+    /**
+     * <p>
+     * Mines a field.
+     * </p>
+     * */
     void mine() {
         if (!hasMine) {
             hasMine = true;
         }
     }
 
+    /**
+     * <p>
+     * Verifies if the field is marked.
+     * </p>
+     *
+     * @return true if it's marked, otherwise returns false.
+     * */
     public boolean isMarked() {
         return isMarked;
     }
 
+    /**
+     * <p>
+     * Verifies if the field is opened.
+     * </p>
+     *
+     * @return true if the field is opened, otherwise returns false
+     * */
     public boolean isOpened() {
         return isOpen;
     }
